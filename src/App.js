6ac6e7c2ab940de04d50ddbfb23a3b5ca1f91fba
vesper85 <utkarshdeshmukh98@gmail.com>
2021-09-29 14:47:58 +0530

    project setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Navbar from './components/Navbar'
 import './App.css';
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Home/>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar/>
+            {/* title */}
+
+            {/* info Widget */}
+
+            {/* How to charity */}
+
+            {/* security and controls */}
+
+            {/* our patners */}
+
+            {/* team info */}
+            
+            {/* footer */}
+        </div>
+    )
+}
+
+export default Home
