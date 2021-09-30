d5359c65cf969054e4b3b99d6a41141c65d41409
vesper85 <utkarshdeshmukh98@gmail.com>
2021-09-30 11:19:27 +0530

    working on map prototype and fixed some bugs

--- src/pages/charityZone.js
+++ src/pages/charityZone.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Map from '../components/Map'
 import './Home.css'
 
 
 const CharityZone = () => {
+    const [state, setstate] = useState('India');
+
+    const handleOnClick = (e)=>{
+        console.log(e.target.getAttribute("title"));
+        setstate(e.target.getAttribute("title"))
+      }
+    
     return (
         <div>
             this is charity zone
+            <h2> {state} </h2>   
             <div className="mapContainer">
-                <Map/>           
+                <Map handleOnClick={handleOnClick}  />    
             </div>
         </div>
     )
