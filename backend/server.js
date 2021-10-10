3538525d59cc069434fb586f727175d54194dce0
vesper85 <utkarshdeshmukh98@gmail.com>
2021-10-10 12:11:44 +0530

    env errors solved

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const port = process.env.port || 5000
 app.use(cors())
 app.use(express.json())
 
-const uri = process.env.ATLAS_URI
+const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
         useNewUrlParser: true,
         useUnifiedTopology: true
