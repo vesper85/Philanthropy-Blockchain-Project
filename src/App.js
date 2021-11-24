e466499816b0cf9e95b093f1b7cc519b73c7864f
RahulSharma7780 <91488292+RahulSharma7780@users.noreply.github.com>
2021-11-24 14:34:36 +0530

    Profile page add

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { Registration } from './pages/Registration';
 import {EditProfile} from './pages/EditProfile'
 import EditCharityForm from './components/EditCharityForm';
 import CharityDetails from './pages/CharityDetails'
+import {Profile} from './pages/Profile'
 
 
 function App() {
@@ -54,6 +55,9 @@ function App() {
           </Route>
           <Route exact path="/charitydetails">
             <CharityDetails/>
+          </Route>
+          <Route exact path="/profile">
+            <Profile/>
           </Route>
         </Switch>
       </Router>
