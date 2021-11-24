e02c8c78889cd1f60d3ad3933cf7915f29fc84c3
Gitesh Patil <patilgitesh29@gmail.com>
2021-11-24 16:23:58 +0530

    Added some more fields in CharityForm

--- src/pages/CharityForm.js
+++ src/pages/CharityForm.js
@@ -50,39 +50,51 @@ export default function CharityForm(props) {
     return (
     <>
         <Navbar/>
-        <div className="reg charity-form">
-            <div className="container_reg">
-                <div className="title_reg">Charity Form</div>
-                    <div className="content_reg">
+        <div className="cf-container charity-form">
+            <div className="cf-container_det">
+                <div className="cf-title">Charity Form</div>
+                    <div className="cf-content">
                     <form onSubmit={onSubmitCharity} onChange={onChangeCharity}>
-                        <div className="user-details_reg">
-                            <div className="input-box_reg" style={{width:"100%"}}>
+                        <div className="user-details">
+                            <div className="input-box" style={{width:"100%"}}>
                                 <span className="details">Charity Name</span>
                                 <input type="text" name="charityName" placeholder="Enter Charity Name" required />
-                            </div>
-                            <div className="input-box_reg" style={{width:"100%"}}>
+                            </div> 
+                            <div className="input-box" style={{width:"100%"}}>
                                 <span className="details">Description</span>
-                                <textarea className="form-control" type="text" name="description" rows="3" placeholder="Enter description" required style={{fontSize:"13px"}}></textarea>
+                                <textarea  type="text" name="description" rows="3" placeholder="Enter description" required style={{fontSize:"13px"}}></textarea>
                             </div>
-                            <div className="input-box_reg" style={{width:"100%"}}>
+                            <div className="input-box" style={{width:"100%"}}>
                                 <span className="details">Cause</span>
                                 <input type="text" name="cause" placeholder="Enter Cause" required />
                             </div>
-                            <div className="input-box_reg">
+                            <div className="input-box">
+                                <span className="details">City</span>
+                                <input type="text" name="city" placeholder="Enter City" required />
+                            </div>
+                            <div className="input-box">
+                                <span className="details">State</span>
+                                <input type="text" name="state" placeholder="Enter State" required />
+                            </div>
+                            <div className="input-box" style={{width:"100%"}}>
                                 <span className="details">Goal</span>
                                 <input type="number" name="goal" placeholder="Enter goal amount" required />
                             </div>
-                            <div className="input-box_reg">
-                                <span className="details">Location</span>
-                                <input type="text" name="location" placeholder="Enter your Location" required />
-                            </div>
-                            <div className="input-box_reg">
+                            <div className="input-box">
                                 <span className="details">Image url</span>
-                                <input type="url" name="imageURL" placeholder="Enter Image url"/>
+                                <input type="url" name="imageURL" placeholder="Enter Image url" required/>
                             </div>
-                            <div className="input-box_reg">
+                            <div className="input-box">
                                 <span className="details">External url</span>
-                                <input type="url" name="externalURL" placeholder="Enter External url"/>
+                                <input type="url" name="externalURL" placeholder="Enter External url" required/>
+                            </div>
+                            <div className="input-box" style={{width:"100%"}}>
+                                <span className="details">Stats</span>
+                                <textarea  type="text" name="stats" rows="3" placeholder="Enter Three Stats" required style={{fontSize:"13px"}}></textarea>
+                            </div>
+                            <div className="input-box" style={{width:"100%"}}>
+                                <span className="details">Notable Work</span>
+                                <textarea  type="text" name="Notable work" rows="3" placeholder="Enter Notable Work" required style={{fontSize:"13px"}}></textarea>
                             </div>
                         </div>
                         <div className="button">
