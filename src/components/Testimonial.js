5265388193d5535cfe8b91a36c79f5b94fc0dfcc
Gitesh Patil <patilgitesh29@gmail.com>
2021-12-12 14:57:42 +0530

    Testimonial card problem fixed

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -5,11 +5,11 @@ function Testimonial() {
         <div className="testimonial-container">
             <div className="container testimonial-wrapper" style={{paddingTop:"40px"}}>
             <div className="d-flex justify-content-center fas fa-heart"></div>
-            <p className="tag text-light text-center">Our users love</p>
-            <h1 className="text-light text-center head pb-3">What we do</h1>
+            <p className="tag text-center">Our users love</p>
+            <h1 className="text-center head pb-3">What we do</h1>
                 <div className="row">
                     <div className="col-lg-4">
-                        <div className="card">
+                        <div className="card test-card">
                             <div className="face front-face"> <img src="https://images.unsplash.com/photo-1557862921-37829c790f19?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80" alt="" className="profile"/>
                                 <div class ="pt-3 text-uppercase name"> Robert Garrison </div>
                                 <div class ="designation">Android Developer</div>
@@ -21,7 +21,7 @@ function Testimonial() {
                         </div>
                     </div>
                     <div className="col-lg-4">
-                        <div className="card">
+                        <div className="card test-card">
                             <div className="face front-face"> <img src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="" className="profile"/>
                                 <div class ="pt-3 text-uppercase name"> Jeffery Kennan </div>
                                 <div class ="designation">Full Stack Developer</div>
@@ -33,7 +33,7 @@ function Testimonial() {
                         </div>
                     </div>
                     <div className="col-lg-4">
-                        <div className="card">
+                        <div className="card test-card">
                             <div className="face front-face"> <img src="https://images.unsplash.com/photo-1614574762522-6ac2fbba2208?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjY2fHxtYW58ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" className="profile"/>
                                 <div class ="pt-3 text-uppercase name"> Issac Maxwell </div>
                                 <div class ="designation">Finance Director</div>
